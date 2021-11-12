b2b9e111b1cccb2a3e9de983ce78985223066598
Shaun <s-purslow@hotmail.com>
2021-11-11 20:33:14 -0700

    update and organize middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const middlewares = require('./middlewares');
+
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+  cors({
+    orgin: 'http://localhost:3000',
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
+
+app.use(middlewares.notFound);
+
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
-
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:1337`);
+  console.log(`Listening at http://localhost:${port}`);
 });
