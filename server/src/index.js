45c4922fc6971d9d3a9a9cc039a0dde93460deb6
Shaun <s-purslow@hotmail.com>
2021-11-12 12:35:03 -0700

    add middleware for api routes

--- server/src/index.js
+++ server/src/index.js
@@ -2,23 +2,36 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const middlewares = require('./middlewares');
+const logs = require('./api/logs');
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(
   cors({
-    orgin: 'http://localhost:3000',
+    orgin: process.env.CORS_ORIGIN,
   })
 );
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World',
   });
 });
+
+app.use('/api/logs', logs);
 
 app.use(middlewares.notFound);
 
